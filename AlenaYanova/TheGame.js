34b64f8839a41d833ba936e461acf3a0a7808c5d
AlenaYanova <leonayanov@gmail.com>
2018-07-09 02:25:26 +0300

    add package.json, change tournament system - add Hero&Hero and Monster&Monster battles

--- AlenaYanova/TheGame.js
+++ AlenaYanova/TheGame.js
@@ -122,7 +122,7 @@ Monster.prototype.getDamage = function() {
 
     if ( this.shouldUseSkill() ) {
         this.skillCounter--;
-        return this.damage*2;
+        return this.damage * 2;
     }
 
     return this.damage;
@@ -153,31 +153,31 @@ function VampireFactory(name) {
 }
 
 
-function Battle(monster, hero) {
-    this.hero = hero;
-    this.monster = monster;
+function Battle(part1, part2) {
+    this.part1 = part1;
+    this.part2 = part2;
 }
 
-Battle.prototype.getHero = function() {
-    return this.hero;
+Battle.prototype.getFirstParticipant = function() {
+    return this.part1;
 }
 
-Battle.prototype.getMonster = function() {
-    return this.monster;
+Battle.prototype.getSecondParticipant = function() {
+    return this.part2;
 }
 
 Battle.prototype.start = function () {
     var
-        hero = this.getHero(),
-        monster = this.getMonster();
-    hero.renewParameters();
-    monster.renewParameters();
-    console.log(hero.getDescription() + ' VS ' + monster.getDescription());
-    while (hero.isAlive() && monster.isAlive()) {
-        hero.attack(monster);
-        console.log('Monster xp: ' + monster.getLife());
-        monster.attack(hero);
-        console.log('Hero xp: ' +hero.getLife());
+        part1 = this.getFirstParticipant(),
+        part2 = this.getSecondParticipant();
+    part1.renewParameters();
+    part2.renewParameters();
+    console.log(part1.getDescription() + ' VS ' + part2.getDescription());
+    while (part1.isAlive() && part2.isAlive()) {
+        part1.attack(part2);
+        console.log('First participant xp: ' + part1.getLife());
+        part1.attack(part2);
+        console.log('Second participant xp: ' + part2.getLife());
     }
 }
 
@@ -186,32 +186,15 @@ function Tournament(numOfParticipants, allowedHeroNames, allowedMonsterNames) {
     this.NUM_OF_PARTICIPANTS = numOfParticipants;
     this.ALLOWED_HERO_NAMES = allowedHeroNames;
     this.ALLOWED_MONSTER_NAMES = allowedMonsterNames;
-    this.monsters = [];
-    this.heroes = [];
+    this.participants = [];
 }
 
-Tournament.prototype.getMonsters = function(){
-    return this.monsters;
+Tournament.prototype.getParticipants = function(){
+    return this.participants;
 }
 
-Tournament.prototype.getHeroes = function(){
-    return this.heroes;
-}
-
-Tournament.prototype.setMonsters = function(newMonstersArr){
-    this.monsters = newMonstersArr;
-}
-
-Tournament.prototype.setHeroes = function(newHeroesArr){
-    this.heroes = newHeroesArr;
-}
-
-Tournament.prototype.addMonster = function(newMonster){
-    this.monsters.push(newMonster);
-}
-
-Tournament.prototype.addHero = function(newHero){
-    this.heroes.push(newHero);
+Tournament.prototype.addParticipant = function(newParticipant){
+    this.participants.push(newParticipant);
 }
 
 Tournament.prototype.faceControl = function(character) {
@@ -221,30 +204,24 @@ Tournament.prototype.faceControl = function(character) {
             return true;
         }
     }
-
     else if (character instanceof Hero) {
         if (_(this.ALLOWED_HERO_NAMES).contains(character.getName())) {
             console.log(character.getDescription() + ' passed the face control!');
             return true;
         }
     }
-
     console.log(character.getDescription() + ' dont passed the face control');
     return false;
 }
 
 Tournament.prototype.addParticipants = function (...arg) {
-    var currentParticipantsNum = this.getHeroes().length + this.getMonsters().length;
-
+    var currentParticipantsNum = this.getParticipants().length;
     for (var i = 0; i < arg.length; i++) {
         if (currentParticipantsNum === this.NUM_OF_PARTICIPANTS) {
             console.log(arg[i].getDescription() + ', sorry, we scored enough participants for the tournament')
         }
         else if (this.faceControl(arg[i])) {
-            if (arg[i] instanceof Hero)
-                this.addHero(arg[i]);
-            else
-                this.addMonster(arg[i]);
+            this.addParticipant(arg[i]);
             currentParticipantsNum++;
         }
     }
@@ -252,77 +229,31 @@ Tournament.prototype.addParticipants = function (...arg) {
 
 Tournament.prototype.start = function () {
     console.log('\n------------ Tournament is started! ------------');
-
     var battleCounter = 0;
-
-    while (this.getMonsters().length !== 0 && this.getHeroes().length !== 0){
-        var heroes = this.getHeroes(),
-            monsters = this.getMonsters(),
-            winHeroes = [],
-            winMonsters = [];
-
-        while (heroes.length !== 0 && monsters.length !== 0){
-            var battle = new Battle(monsters.shift(), heroes.shift());
-            battleCounter++;
-            console.log('\n---- Battle ' + battleCounter + ' ----');
-            battle.start();
-            if (battle.getMonster().isAlive()) {
-                winMonsters.push(battle.getMonster());
-                console.log('Monster win this battle');
-            }
-            else if (battle.getHero().isAlive()) {
-                winHeroes.push(battle.getHero());
-                console.log('Hero win this battle');
-            }
-            else
-                console.log('All characters die')
+    var participants = _.shuffle(this.getParticipants());
+    while (participants.length !== 0 && participants.length !== 1){
+        var battle = new Battle(participants.shift(), participants.shift());
+        battleCounter++;
+        console.log('\n---- Battle ' + battleCounter + ' ----');
+        battle.start();
+        if (battle.getFirstParticipant().isAlive()) {
+            participants.push(battle.getFirstParticipant());
+            console.log(battle.getFirstParticipant().getDescription() + ' win this battle');
         }
-
-        // console.log(heroes.concat(winHeroes));
-        // break;
-        this.setHeroes(heroes.concat(winHeroes));
-        this.setMonsters(monsters.concat(winMonsters));
+        else if (battle.getSecondParticipant().isAlive()) {
+            participants.push(battle.getHero());
+            console.log(battle.getSecondParticipant().getDescription() + ' win this battle');
+        }
+        else
+            console.log('All characters die')
     }
-
     console.log('\n\nRESULT: ');
-    this.showWinner();
+    if (participants.length === 1)
+        console.log(participants[0].getDescription() + ' is winner!');
+    else
+        console.log('Everybody die!');
     console.log('\n------------ Tournament is ended! ------------')
 };
-
-Tournament.prototype.showWinner = function () {
-    var heroes = this.getHeroes(),
-        monsters = this.getMonsters();
-
-    if (monsters.length !== 0 && heroes.length !== 0)
-        console.log('Sorry, the result can not be shown, the tournament is not over');
-
-    else if (monsters.length !== 0){
-        if (monsters.length === 1)
-            console.log(monsters[0].getDescription() + ' is winner!');
-        else {
-            console.log('All this monsters are alive: ');
-            monsters.forEach(function (monster) {
-                console.log(monster.getDescription())
-            })
-        }
-    }
-
-    else if (heroes.length){
-        if (heroes.length === 1)
-            console.log(heroes[0].getDescription() + ' is winner!');
-        else {
-            console.log('All this heroes are alive: ');
-            heroes.forEach(function (hero) {
-                console.log(hero.getDescription())
-            })
-        }
-    }
-
-    else
-        console.log('Everybody die!')
-
-}
-
 
 // ---------------- It's time to test this code! ----------------
 
